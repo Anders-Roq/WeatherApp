6f1016dac75944b74e6f2d8421621419759bf06d
marcelo <mcpnsh@gmail.com>
2026-05-28 14:42:40 -0300

    Commit - Parte 1: Criando as novas telas

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,22 +1,14 @@
 package com.example.weatherapp
 
-import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.example.weatherapp.ui.HomePage
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,13 +27,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun HomePage(modifier: Modifier = Modifier) {
-    val activity = LocalContext.current as Activity
-    Column(modifier = modifier.fillMaxSize().padding(24.dp)) {
-        Text("Bem-vindo/a!", fontSize = 24.sp)
-        Button(onClick = { activity.finish() }) {
-            Text("Sair")
-        }
-    }
-}
